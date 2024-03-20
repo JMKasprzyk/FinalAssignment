cc3d8688adf1c8c6b1743f35de3e4ad48d96a437
Jakub Kasprzyk <jakub.m.kasprzyk@gmail.com>
2024-03-20 14:25:44 +0100

    Remove the prints from the DiceLoss class

--- losses.py
+++ losses.py
@@ -26,9 +26,6 @@ class DiceLoss(nn.Module):
             class_loss = 1 - ((2. * intersection + smooth) / (input_flat.sum() + target_flat.sum() + smooth))
             dice_loss += class_loss
 
-            print(f"Loss for class {class_index}: {class_loss.item()}")
-            print(f"Aggregated loss: {dice_loss.item()}")
-
         mean_dice = dice_loss/input.size(1) # average loss over all classes
 
         if self.log_loss:
