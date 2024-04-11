707ef1b851721fa35d9f09d6148b43883353d50a
Jakub Kasprzyk <jakub.m.kasprzyk@gmail.com>
2024-04-11 22:06:50 +0200

    Createsnellius job testing comb CE+Dice loss on Att UNet

--- losses.py
+++ losses.py
@@ -2,6 +2,7 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 from torch.nn.modules.loss import _Loss
+from utils import LABELS
 
 class DiceLoss(nn.Module):
     def __init__(self, ignore_index=255, log_loss=False):
@@ -94,3 +95,42 @@ class FocalLoss(_Loss):
         # print(f"Focal loss: {loss}")
 
         return loss
+    
+class CE_Dice_Loss(nn.Module):
+    """
+    Combined CrossEntropy and Dice Loss: The CrossEntropyLoss is calculated for all classes,
+    while the DiceLoss is calculated only for the "object" category classes. The final loss is 
+    the sum of the CrossEntropyLoss and the mean DiceLoss for the "object" category classes.
+
+    the idea behind this loss is to help the CrossEntropyLoss to focus on the "object" category classes.
+    """
+    def __init__(self, ce_weight=None, ignore_index=255, log_dice_loss=False):
+        super(CE_Dice_Loss, self).__init__()
+        self.ce_weight = ce_weight
+        self.dice_loss = DiceLoss(ignore_index=ignore_index, log_loss=log_dice_loss)
+        self.cross_entropy = nn.CrossEntropyLoss(weight=ce_weight, ignore_index=ignore_index)
+
+    def forward(self, input, target):
+        # Calculate CrossEntropyLoss
+        ce_loss = self.cross_entropy(input, target)
+
+        # Get the classes in the "object" category
+        object_classes = [label.trainId for label in LABELS if label.category == 'object']
+
+        # Calculate DiceLoss only for the "object" category classes
+        dice_loss = 0.
+        smooth = 1.
+
+        for class_index in object_classes:
+            valid = (target == class_index)
+            input_flat = input[:, class_index, :, :][valid].contiguous().view(-1)
+            target_flat = (target == class_index)[valid].contiguous().view(-1)  # binary target for class_index
+
+            intersection = (input_flat * target_flat).sum()
+
+            class_loss = 1 - ((2. * intersection + smooth) / (input_flat.sum() + target_flat.sum() + smooth))
+            dice_loss += class_loss
+
+        mean_dice = dice_loss / len(object_classes) if len(object_classes) > 0 else 0.
+
+        return ce_loss + mean_dice
