10b13f1740632940d5673df9ae660587e861f216
Jakub Kasprzyk <jakub.m.kasprzyk@gmail.com>
2024-04-20 16:01:06 +0200

    Create snellius job for the Att UNet with Focla loss.

--- train.py
+++ train.py
@@ -7,9 +7,9 @@ import torch
 # from MS_UNet import MSU_Net
 # from R2_UNet import R2U_Net
 # from R2AttU_Net import R2AttU_Net
-from ResUNet import ResUNet
+#from ResUNet import ResUNet
 #from Res_Att_UNet import ResAttU_Net
-#from Att_UNet import Att_UNet
+from Att_UNet import Att_UNet
 from model_executables import train_model_wandb
 import losses as L
 from torchvision.datasets import Cityscapes
@@ -58,7 +58,7 @@ def main(args):
     val_loader = torch.utils.data.DataLoader(validation_dataset, batch_size=40, shuffle=True, num_workers=8)
 
     # Instanciate the model
-    UNet_model = ResUNet()
+    UNet_model = Att_UNet()
 
     # Move the model to the GPu if avaliable
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
