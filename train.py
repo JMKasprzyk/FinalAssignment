e73f6724cf83643e48b46d77baae068f4c1a26f3
Jakub Kasprzyk <jakub.m.kasprzyk@gmail.com>
2024-04-20 08:58:55 +0200

    Create a snellius job for the UNet with CE

--- train.py
+++ train.py
@@ -3,13 +3,13 @@ This file needs to contain the main training loop. The training code should be e
 avoid any global variables.
 """
 import torch
-# from model import Model
+from model import Model
 # from MS_UNet import MSU_Net
 # from R2_UNet import R2U_Net
 # from R2AttU_Net import R2AttU_Net
 # from ResUNet import ResUNet
 # from Res_Att_UNet import ResAttU_Net
-from Att_UNet import Att_UNet
+#from Att_UNet import Att_UNet
 from model_executables import train_model_wandb
 import losses as L
 from torchvision.datasets import Cityscapes
@@ -54,11 +54,11 @@ def main(args):
     training_dataset, validation_dataset = torch.utils.data.random_split(training_dataset, [train_size, val_size])
 
     # Create Training and Validation DataLoaders
-    train_loader = torch.utils.data.DataLoader(training_dataset, batch_size=32, shuffle=True, num_workers=8)
-    val_loader = torch.utils.data.DataLoader(validation_dataset, batch_size=32, shuffle=True, num_workers=8)
+    train_loader = torch.utils.data.DataLoader(training_dataset, batch_size=40, shuffle=True, num_workers=8)
+    val_loader = torch.utils.data.DataLoader(validation_dataset, batch_size=40, shuffle=True, num_workers=8)
 
     # Instanciate the model
-    UNet_model = Att_UNet()
+    UNet_model = Model()
 
     # Move the model to the GPu if avaliable
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
@@ -69,18 +69,18 @@ def main(args):
     #     UNet_model = torch.nn.DataParallel(UNet_model)
 
     # define optimizer and loss function (don't forget to ignore class index 255)
-    # criterion = nn.CrossEntropyLoss(ignore_index=255)
+    criterion = nn.CrossEntropyLoss(ignore_index=255)
     # criterion = L.DiceLoss(ignore_index=255)
     # criterion = L.FocalLoss(ignore_index=255)
     # criterion = L.CE_Dice_Loss(ignore_index=255)
-    criterion = L.CE_FL_Dice_Loss(ignore_index=255)
+    # criterion = L.CE_FL_Dice_Loss(ignore_index=255)
     optimizer = optim.Adam(UNet_model.parameters(), lr=args.lr)
 
 
     # start a new wandb run to track this script
     wandb.init(
         # set the wandb project where this run will be logged
-        project = "5LSM0-WB-UNet-train",
+        project = "5LSM0-WB-UNet-report",
         name = args.wandb_name,  # the name of the run is the same as the name of the script
         # track hyperparameters and run metadata
         config={
