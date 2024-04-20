e3ff9682180fb52861736d7e826649a58cfc44a7
Jakub Kasprzyk <jakub.m.kasprzyk@gmail.com>
2024-04-20 15:57:51 +0200

    Create snellius job for Res UNet with Focal loss.

--- run_container.sh
+++ run_container.sh
@@ -8,6 +8,6 @@ wandb login
 torchrun --nnodes=1 --nproc_per_node=1 train.py \
         --data_path "/gpfs/work5/0/jhstue005/JHS_data/CityScapes"\
         --epochs 150 --lr 0.01\
-        --wandb_name "Res-UNet-Dice"\
-        --checkpoint_folder "checkpoints/report/Res-UNet-Dice"\
+        --wandb_name "Res-UNet-FL"\
+        --checkpoint_folder "checkpoints/report/Res-UNet-FL"\
         --architecture "Res-UNet"
