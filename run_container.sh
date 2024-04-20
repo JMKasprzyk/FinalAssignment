ee6cc4faf45b42442358d4516e95683965a1270f
Jakub Kasprzyk <jakub.m.kasprzyk@gmail.com>
2024-04-20 09:11:03 +0200

    Create snellius job for UNet with Dice loss.

--- run_container.sh
+++ run_container.sh
@@ -8,6 +8,6 @@ wandb login
 torchrun --nnodes=1 --nproc_per_node=1 train.py \
         --data_path "/gpfs/work5/0/jhstue005/JHS_data/CityScapes"\
         --epochs 150 --lr 0.01\
-        --wandb_name "UNet-CE"\
-        --checkpoint_folder "checkpoints/report/UNet-CE"\
+        --wandb_name "UNet-Dice"\
+        --checkpoint_folder "checkpoints/report/UNet-Dice"\
         --architecture "UNet"
