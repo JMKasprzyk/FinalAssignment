7e8e1481ec687d368b43a280779202ebd7a36a9c
Jakub Kasprzyk <jakub.m.kasprzyk@gmail.com>
2024-04-21 03:17:04 +0200

    Create snellius job for MSU-Net with Focal loss.

--- run_container.sh
+++ run_container.sh
@@ -8,6 +8,6 @@ wandb login
 torchrun --nnodes=1 --nproc_per_node=1 train.py \
         --data_path "/gpfs/work5/0/jhstue005/JHS_data/CityScapes"\
         --epochs 150 --lr 0.01\
-        --wandb_name "MSU-Net-Dice"\
-        --checkpoint_folder "checkpoints/report/MSU-Net-Dice"\
+        --wandb_name "MSU-Net-FL"\
+        --checkpoint_folder "checkpoints/report/MSU-Net-FL"\
         --architecture "MSU-Net"
